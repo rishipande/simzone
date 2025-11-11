78b1de3d2e72e94b3c3e1bb392f75bd4adf39fa0
Rishi Pande <rishi@fryol.com>
2025-11-11 15:52:51 -0800

    Initial Commit

---
+++ Simzone/SimzoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimzoneApp.swift
+//  Simzone
+//
+//  Created by Rishi on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SimzoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
